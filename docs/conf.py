0b96baa1b082d15700496db0ab9817abc1088c70
mehrab <memahdian@outlook.com>
2025-07-03 18:46:49 +0300

    update docs conf

--- docs/conf.py
+++ docs/conf.py
@@ -8,37 +8,28 @@ sys.path.insert(0, os.path.abspath('../sw'))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'py-pid'
 copyright = '2025, Mehrab Mahdian'
 author = 'Mehrab Mahdian'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',  # for Google-style or NumPy docstrings
 ]
+
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 
 html_context = {
-    "display_github": True,
-    "github_user": "mehrabmahdian",
-    "github_repo": "py-pid",
-    "github_version": "main",
-    "conf_py_path": "/",
+    "display_github": True,              # Show GitHub link in the docs
+    "github_user": "mehrabmahdian",     # Your GitHub username
+    "github_repo": "py-pid",             # Your GitHub repo name
+    "github_version": "main",             # Branch name
+    "conf_py_path": "/",                  # Path in the repo to your docs root
 }
-
-
 
 html_static_path = ['_static']
